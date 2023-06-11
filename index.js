acf2dfd274fddd2812e0ab42457a08227f13251d
sourovmodak06 <sourovmodak650@gmail.com>
2023-06-11 18:10:34 +0600

    secure email

--- index.js
+++ index.js
@@ -83,6 +83,16 @@ app.post("/users", async (req, res) => {
   const result = await userCollection.insertOne(user);
   res.send(result);
 });
+app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+  const email = req.params.email;
+  if (req.decoded.email !== email) {
+    res.send({ admin: false });
+  }
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const result = { admin: user?.role === "admin" };
+  res.send(result);
+});
 app.patch("/users/admin/:id", async (req, res) => {
   const id = req.params.id;
   const filter = { _id: new ObjectId(id) };
