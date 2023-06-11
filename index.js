6345c74bbd32f9158b572447e382f734e16b11a7
sourovmodak06 <sourovmodak650@gmail.com>
2023-06-11 17:34:08 +0600

    verifyJWT token created

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.boafyub.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -97,10 +118,14 @@ app.get("/classes", async (req, res) => {
     .toArray();
   res.send(result);
 });
-app.get("/classCart", async (req, res) => {
+app.get("/classCart", verifyJWT, async (req, res) => {
   const email = req.query.email;
   if (!email) {
     res.send([]);
+  }
+  const decodedEmail = req.decoded.email;
+  if (email !== decodedEmail) {
+    return res.status(403).send({ error: true, message: "forbidden access" });
   }
   const query = { email: email };
   const result = await classCartCollection.find(query).toArray();
