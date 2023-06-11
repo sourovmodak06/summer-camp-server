9b7c8c403c4c38545ba461eb66c2445e2b1b017e
sourovmodak06 <sourovmodak650@gmail.com>
2023-06-11 21:00:05 +0600

    Deleted user

--- index.js
+++ index.js
@@ -96,6 +96,12 @@ app.post("/users", async (req, res) => {
   const result = await userCollection.insertOne(user);
   res.send(result);
 });
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await userCollection.deleteOne(query);
+  res.send(result);
+});
 app.get("/users/admin/:email", verifyJWT, async (req, res) => {
   const email = req.params.email;
   if (req.decoded.email !== email) {
